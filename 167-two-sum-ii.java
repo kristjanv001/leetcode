84dc2749644f193dd2919f077ce2205d76665d2f
Kristjan Vingel <56689530+kristjanv001@users.noreply.github.com>
2021-03-12 08:35:18 +0200

    Update 167-two-sum-ii.java

--- 167-two-sum-ii.java
+++ 167-two-sum-ii.java
@@ -1,16 +1,23 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length-1;
         
         int[] indecies = new int[2];
-        HashMap<Integer, Integer> hashmap = new HashMap<>();
         
-        for (int i = 0; i < numbers.length; i++) {
-            if (hashmap.containsKey(target-numbers[i])) {
-                indecies[0] = hashmap.get(target-numbers[i])+1;
-                indecies[1] = i+1;         
+        while (left <= right) {
+            
+            int sum = numbers[left] + numbers[right];
+            
+            if (sum > target) {
+                right--;
+            } else if(sum < target) {
+                left++;
             } else {
-                hashmap.put(numbers[i], i);
-            }    
+                indecies[0] = left + 1;
+                indecies[1] = right + 1;
+                return indecies;
+            }       
         }
         return indecies;
     }
