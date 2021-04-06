5ce910d2442e010c5d4b1720e8a5e50e26a293c8
Kristjan Vingel <56689530+kristjanv001@users.noreply.github.com>
2021-04-06 12:14:59 +0300

    Update 0283_move-zeroes.java

--- 0283_move-zeroes.java
+++ 0283_move-zeroes.java
@@ -1,9 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int read = 0;
-        int write = 0;
         
-        while (read <= nums.length - 1) {
+        int read = 0; // fast
+        int write = 0; // slow
+        
+        // First loop, we keep moving the read pointer 
+        // until it encounters a number that is not 0. 
+        // Then we write the value at write and 
+        // increment the write pointer. 
+        // We increment the read pointer in any case.
+        while (read < nums.length) {
             if (nums[read] != 0) {
                 nums[write] = nums[read];
                 write++;
@@ -11,10 +17,11 @@ class Solution {
             read++;
         }
         
-        while (write <= nums.length - 1) {
+        // Second loop, we just overwrite all the 
+        // remaining elements with zeroes
+        while (write < nums.length) {
             nums[write] = 0;
             write++;
         }
-        
     }
 }
